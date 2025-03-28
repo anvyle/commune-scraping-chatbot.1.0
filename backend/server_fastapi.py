452709854296740bbce544753f4e0d444ea63a71
pyGirrafe <devgold59@gmail.com>
2025-03-29 00:46:07 +0700

    update

--- backend/server_fastapi.py
+++ backend/server_fastapi.py
@@ -27,6 +27,7 @@ logger = logging.getLogger(__name__)
 def index():
     return 'Welcome to the FastAPI server!'
 
+
 @app.get('/api/scrape')
 async def scrape_website_embed(website_url: str):
     try:
