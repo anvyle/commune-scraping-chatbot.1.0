aad98a5978bde2dff4d6804c939ec6699a6e7f15
git-anatolii <lysenkoanatolii41@gmail.com>
2025-03-29 20:15:59 +0700

    Update search.py
    Co-authored-by: @git-anatolii  <lysenkoanatolii41@gmail.com>

--- backend/search.py
+++ backend/search.py
@@ -12,13 +12,13 @@ parser = argparse.ArgumentParser()
 parser.add_argument("search_string", type=str)
 
 if __name__ == "__main__":
-    # if not os.path.exists("./chroma"):
-    #     print("No chroma persisted, run `python embed.py` to create one")
-    #     exit(0)
-    # args = parser.parse_args()
-    # embeddings = OpenAIEmbeddings(model="text-embedding-ada-002")
-    # db = Chroma(persist_directory="./chroma", embedding_function=embeddings)
-    # docs = db.similarity_search(args.search_string)
-    # for doc in docs[0:5]:
-    #     print(doc)
-    #     print("\n")
+    if not os.path.exists("./chroma"):
+        print("No chroma persisted, run `python embed.py` to create one")
+        exit(0)
+    args = parser.parse_args()
+    embeddings = OpenAIEmbeddings(model="text-embedding-ada-002")
+    db = Chroma(persist_directory="./chroma", embedding_function=embeddings)
+    docs = db.similarity_search(args.search_string)
+    for doc in docs[0:5]:
+        print(doc)
+        print("\n")
